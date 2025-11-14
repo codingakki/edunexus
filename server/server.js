59b52583bfb11f306b6cd2db34fe7ef7c11ecc27
codingakki <aryanshrivastava0038@gmail.com>
2025-11-15 00:28:04 +0530

    2

--- server/server.js
+++ server/server.js
@@ -34,9 +34,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // ===============================================
 app.use(
   "/private_notes",
-  express.static(path.join(__dirname, "../private_notes"))
+  express.static(path.join(__dirname, "../../private_notes"))
 );
-
 
 // ===============================================
 // MongoDB Connection
